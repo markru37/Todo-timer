e6da86294b25840f5b4619edd17e0d08c11ce0de
Mark_Rumjantsev <mrk.ru@list.ru>
2022-08-12 15:28:19 +0300

    ts

--- todo-list/src/index.tsx
+++ todo-list/src/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <Provider store={store}>
         <BrowserRouter>
@@ -15,4 +13,3 @@ root.render(
     </Provider>,
 );
 
-reportWebVitals();
